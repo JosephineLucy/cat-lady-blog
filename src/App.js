aaba9e2281c0ae3f83d421ade30e8c09217d6d52
Josephine Lindley <Josephine.Lindley@MacBook-Pro-2.local>
2023-01-16 16:47:19 +0000

    adds create component and useFetch custom hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Create from './components/Create';
 
 function App() {
 
   return (
+    <Router>
     <div className="App">
       <Navbar/>
       <div className='content'>
-      <Home/>
+        <Switch>
+          <Route exact path={'/'}>
+            <Home/>
+          </Route>
+          <Route exact path={'/create'}>
+            <Create/>
+          </Route>
+
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
