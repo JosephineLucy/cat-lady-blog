cab41ca24e21f5c43046da7e7f6587b5fe1d9a4a
Josephine Lindley <Josephine.Lindley@MacBook-Pro-2.local>
2023-01-17 17:05:19 +0000

    adds create form

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,7 +1,41 @@
+import { useState } from "react";
 const Create = () => {
+    const [ title, setTitle ] = useState('');
+    const [ body, setBody ] = useState('');
+    const [ author, setAuthor ] = useState('Cat Lady')
+
     return ( 
         <div className="create">
             <h2>Add a new blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input 
+                type="text"
+                required 
+                value={ title }
+                onChange={ (e)=>setTitle(e.target.value) }
+                />
+                <label>Blog body:</label>
+                <textarea
+                value={ body }
+                onChange={ (e)=>setBody(e.target.value) }
+                required
+                ></textarea>
+                <label>Blog author:</label>
+                <select
+                    value={ author }
+                    onChange={ (e)=>setAuthor(e.target.value) }
+                >
+                    <option value="Cat Lady">Cat Lady</option>
+                    <option value="Rupert">Rupert</option>
+                    <option value="Harry">Harry</option>
+                    <option value="Catman">Catman</option>
+                </select>
+                <button>Add blog</button>
+            </form>
+            <p>{ title }</p>
+            <p>{ body }</p>
+            <p>{ author }</p>
         </div>
      );
 }
